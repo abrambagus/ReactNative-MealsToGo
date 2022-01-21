ca1f74b466302f94b0141fcc7a0207c35320d3ef
abrambagus <you@example.com>
2022-01-21 18:21:34 +0700

    login-screen

--- App.js
+++ App.js
@@ -6,24 +6,12 @@ import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
-import { initializeApp } from "firebase/app";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCGYTTf4m1zwetz1v-PWMThxuxexyxHSRY",
-  authDomain: "mealstogo-3db07.firebaseapp.com",
-  projectId: "mealstogo-3db07",
-  storageBucket: "mealstogo-3db07.appspot.com",
-  messagingSenderId: "880032011398",
-  appId: "1:880032011398:web:cf2ea2a65774b4b6cf2d07",
-};
-
-initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,7 +1,27 @@
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGYTTf4m1zwetz1v-PWMThxuxexyxHSRY",
+  authDomain: "mealstogo-3db07.firebaseapp.com",
+  projectId: "mealstogo-3db07",
+  storageBucket: "mealstogo-3db07.appspot.com",
+  messagingSenderId: "880032011398",
+  appId: "1:880032011398:web:cf2ea2a65774b4b6cf2d07",
+};
+
+initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
 export const loginRequest = (email, password) => {
-  signInWithEmailAndPassword(auth, email, password);
+  return new Promise((resolve, reject) => [
+    signInWithEmailAndPassword(auth, email, password)
+      .then((u) => {
+        resolve(u);
+      })
+      .catch((e) => {
+        reject(e);
+      }),
+  ]);
 };

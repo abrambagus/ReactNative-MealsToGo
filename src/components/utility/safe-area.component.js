8b34f552e6f25a0ca65857624824f68c20abeaaf
abrambagus <you@example.com>
2022-01-22 16:01:29 +0700

    add-watermelon-animation

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -1,7 +1,8 @@
-import { StatusBar } from "react-native";
+import { StatusBar, SafeAreaView } from "react-native";
 import styled from "styled-components/native";
 
-export const SafeArea = styled.SafeAreaView`
+export const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
